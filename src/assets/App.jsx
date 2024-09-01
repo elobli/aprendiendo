fe7fd02e8df28435a0918f49fbc8d29acfe31eb9
elobli <oblisestrada@gmail.com>
2024-08-31 20:54:22 -0400

    mejroas

--- src/assets/App.jsx
+++ src/assets/App.jsx
@@ -1,13 +1,14 @@
 export function App() {
     return(
-        <article style={{ display: 'flex', alignItems:'center', color:'#fff'}}>
+        <article style={{ display: 'flex', alignItems:'center', color:'#fff' }}>
             <header>
-                <img alt="El avatar de mideudev" src="https://imgv3.fotor.com/images/share/fotor-ai-generate-a-lifelike-dragon.jpg" />
+                <img alt="El avatar de mideudev" src="https://imgv3.fotor.com/images/share/fotor-ai-generate-a-lifelike-dragon.jpg" width="500"/>
                 <div>
                     <strong>Miguel Angel Duran</strong>
                     <span>@oblis</span>
                 </div>
             </header>
+
             <aside>
                 <button>
                     seguir
