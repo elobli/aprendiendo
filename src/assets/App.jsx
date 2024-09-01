dcd0a6e609146b9446b5fadb06443ce2937789eb
elobli <oblisestrada@gmail.com>
2024-08-31 20:34:38 -0400

    twttiwer

--- src/assets/App.jsx
+++ src/assets/App.jsx
@@ -1,8 +1,18 @@
-function App() {
+export function App() {
     return(
-        <div>
-            <h1>Twitter Card</h1>
-
-        </div>
+        <article>
+            <header>
+                <img src="https://imgv3.fotor.com/images/share/fotor-ai-generate-a-lifelike-dragon.jpg" />
+                <div>
+                    <strong>Miguel Angel Duran</strong>
+                    <span>@oblis</span>
+                </div>
+            </header>
+            <aside>
+                <button>
+                    seguir
+                </button>
+            </aside>
+        </article>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React  from 'react'
 import ReactDOM from 'react-dom/client'
-
+import { App } from './assets/App'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 //Siempre PascalCase
 
 root.render(
-<div>
-
-</div>)
+    <App />  
+)
